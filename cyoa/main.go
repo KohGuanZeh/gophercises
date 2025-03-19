6ee4888ab2f7674c6e42881209040ef96141a155
Koh Guan Zeh <kohguanzeh@gmail.com>
2025-03-19 22:28:30 +0800

    Add http handler

--- cyoa/main.go
+++ cyoa/main.go
@@ -5,8 +5,10 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strings"
+	"text/template"
 )
 
 type Arc struct {
@@ -21,27 +23,22 @@ type Option struct {
 }
 
 func main() {
-	filename := parseFlags()
+	filename, templateName := parseFlags()
 	arcs := getArcsFromFile(filename)
-	for key, value := range arcs {
-		fmt.Printf("Arc: %s\n", key)
-		fmt.Printf("Title: %s\n", value.Title)
-		for _, story := range value.Story {
-			fmt.Println(story)
-		}
-		for i, option := range value.Options {
-			fmt.Printf("Option %d: %s (arc: %s)\n", i, option.Text, option.Arc)
-		}
-		fmt.Println()
-	}
+	tmpl := getHtmlTemplate(templateName)
+	storyArcHandler := createStoryArcHandler(arcs, tmpl)
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", storyArcHandler)
 }
 
-func parseFlags() string {
+func parseFlags() (string, string) {
 	filenamePtr := flag.String("file", "gopher.json", "Json file name containing stories. Default is `gopher.json`.")
 	flag.StringVar(filenamePtr, "f", *filenamePtr, "Alias for --file.")
+	templateNamePtr := flag.String("template", "template.html", "HTML file for rendering template. Default is `template.html`.")
+	flag.StringVar(templateNamePtr, "t", *templateNamePtr, "Alias for --template.")
 	flag.Parse()
 
-	return strings.TrimSpace(*filenamePtr)
+	return strings.TrimSpace(*filenamePtr), strings.TrimSpace(*templateNamePtr)
 }
 
 func getArcsFromFile(filename string) map[string]Arc {
@@ -55,4 +52,36 @@ func getArcsFromFile(filename string) map[string]Arc {
 		log.Fatalf("Error parsing Json: %s\n", err)
 	}
 	return arcs
+}
+
+func getHtmlTemplate(templateName string) *template.Template {
+	tmpl, err := template.ParseFiles(templateName)
+	if err != nil {
+		log.Fatalf("Error parsing HTML template: %s\n", err)
+	}
+	return tmpl
+}
+
+func createStoryArcHandler(arcs map[string]Arc, tmpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		key := strings.TrimPrefix(r.RequestURI, "/")
+		if key == "style.css" {
+			// Only serve style.css file
+			http.ServeFile(w, r, key)
+			return
+		}
+		if key == "" {
+			// Set intro as default
+			key = "intro"
+		}
+		arc, ok := arcs[key]
+		if !ok {
+			http.NotFound(w, r)
+			return
+		}
+		err := tmpl.Execute(w, arc)
+		if err != nil {
+			log.Fatalf("Error rendering HTML: %s\n", err)
+		}
+	}
 }
