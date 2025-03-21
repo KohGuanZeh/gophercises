6182fc9562b12ac39d8cc99ea3df64dbd4b61f24
Koh Guan Zeh <kohguanzeh@gmail.com>
2025-03-21 15:19:17 +0800

    Update main to use linkparser package

--- linkparser/main.go
+++ linkparser/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"gophercises/linkparser/linkparser"
 	"log"
 	"os"
 	"strings"
@@ -10,15 +11,10 @@ import (
 	"golang.org/x/net/html"
 )
 
-type Link struct {
-	Href string
-	Text string
-}
-
 func main() {
 	filename := parseFlags()
 	node := parseHtmlFile(filename)
-	links := parseLinks(node)
+	links := linkparser.ParseLinks(node)
 	printLinks(links)
 }
 
@@ -42,43 +38,7 @@ func parseHtmlFile(filename string) *html.Node {
 	return node
 }
 
-// Nested links will be extracted as separate link.
-// Upon rendering through HTML, nested links are created as sibling.
-// Try opening up test.html and you see that the nested link is a sibling.
-func parseLinks(node *html.Node) []Link {
-	if node.Type == html.ElementNode && node.Data == "a" {
-		href := ""
-		for _, attr := range node.Attr {
-			if attr.Key == "href" {
-				href = attr.Val
-				break
-			}
-		}
-		var sb strings.Builder
-		for next := node.FirstChild; next != nil; next = next.NextSibling {
-			sb.WriteString(getAnchorText(next))
-		}
-		return []Link{{Href: href, Text: sb.String()}}
-	}
-	links := make([]Link, 0)
-	for next := node.FirstChild; next != nil; next = next.NextSibling {
-		links = append(links, parseLinks(next)...)
-	}
-	return links
-}
-
-func getAnchorText(node *html.Node) string {
-	if node.Type == html.TextNode {
-		return strings.TrimSpace(node.Data)
-	}
-	var sb strings.Builder
-	for next := node.FirstChild; next != nil; next = next.NextSibling {
-		sb.WriteString(getAnchorText(next))
-	}
-	return sb.String()
-}
-
-func printLinks(links []Link) {
+func printLinks(links []linkparser.Link) {
 	fmt.Println("\nLinks found:", len(links))
 	fmt.Println()
 	for _, link := range links {
